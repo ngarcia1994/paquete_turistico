1fd127ab85b52e55140653589f399e396ccaa269
Andres Castrillon <andres.castrillon@alegra.com>
2021-09-11 14:36:07 -0500

    se crea capa de comunicación con firebase

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # project-utp
 
 ## Project setup
+
+Dubplicar el archivo .env.example con el nombre .env con la información necesario para configurar firebase.
+
+Correr el siguiente comando para instalar las dependencias necesarias:
 ```
 npm install
 ```
@@ -22,3 +26,12 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Firebase
+
+Se agregó una clase que se encargará de conectar realizar las operaciones de lectura y escritura en farebase estos son:
+
+```
+retrieveAll(documentName') => promise
+addDocument(documentName, ObjectInfo) => promise
+```
--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,34 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  <button @click="add">Click aquí</button>
 </template>
 
+<script>
+// import {retrieveAll, addDocument} from './utils/dbHandler';
+
+export default {
+    name: "App",
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+      // Obtiene todos los datos
+      // retrieveAll('user').then(data => {
+      //     console.log(data);
+      // });
+
+      // Agrega un documento con la información pasada
+      // addDocument('user', {name: 'Test'})
+
+    },
+    methods: {
+
+    }
+  }
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
